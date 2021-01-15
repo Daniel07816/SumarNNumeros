bde6199184279404002733dc8dcfa7894c842b45
Daniel07816 <danielbehar@ufm.edu>
2021-01-15 15:54:57 -0600

    termine el documento testing y agregue la imagen del profiling y video del test

--- testing.py
+++ testing.py
@@ -4,7 +4,7 @@ import main
 class TestMyModule(unittest.TestCase):
     
     def test_sum(self):
-        self.assertEqual(main.sum(4, 5), 5)
+        self.assertEqual(main.sumador(6), 21)
         
 if __name__ == "__main__":
     unittest.main()
